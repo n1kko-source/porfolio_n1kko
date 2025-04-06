b12a1fdebb2b2ef55bf396af513d5fc1e814c1c0
n1kko-source <juannmunoz20@gmail.com>
2025-04-06 16:28:11 -0500

    Trigger deploy

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contact  from './components/Contact'
 
 export const App = () => {
   return (
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+    <div className='overflow-x-hidden overflow-y-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
 
       <div className='fixed top-0 -z-10 h-full w-full'>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 
